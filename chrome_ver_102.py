3ddf7df249ca236d4ea8e4e64fa4cc93973ec73d
Bari94 <74244650+Bari94@users.noreply.github.com>
2022-06-15 22:12:25 +0900

    コメント追加

--- chrome_ver_102.py
+++ chrome_ver_102.py
@@ -2,6 +2,7 @@ import time
 import datetime
 from selenium import webdriver
 
+#webサイト
 chrome = webdriver.Chrome(executable_path = 'C:/Users/利光/Desktop/プログラム/python/chromedriver.exe')
 chrome.get("https://www.arealme.com/click-speed-test/ja/")
 
@@ -11,10 +12,10 @@ chrome.find_element_by_id('start').click()
 time.sleep(1)
 chrome.find_element_by_id('clickarena').click()
 min10 = datetime.datetime.now() + datetime.timedelta(seconds = 10)
-
+#10秒間連打
 while datetime.datetime.now() <= min10:
     chrome.find_element_by_id('clickarena').click()
-        
+    #3秒待って閉じる    
 time.sleep(3)
 chrome.quit()
 
